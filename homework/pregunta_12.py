45097826627de1e59bd85ea4436d250ac2f7674a
CloaizaF <46464898+CloaizaF@users.noreply.github.com>
2025-05-12 19:31:06 -0500

    Finish pregunta 12

--- homework/pregunta_12.py
+++ homework/pregunta_12.py
@@ -19,13 +19,18 @@ def pregunta_12():
     with open("files/input/data.csv", "r", newline="") as archivo_csv:
         lector = csv.reader(archivo_csv, delimiter="\t")
         for fila in lector:
-            letras = fila[3].split(",")
+            letraC1 = fila[0]
+            letras = fila[4].split(",")
             for letra in letras:
-                if letra in dic:
-                    dic[letra] += int(fila[1])
+                clave, valor = letra.split(":")
+                if letraC1 in dic:
+                    dic[letraC1] += int(valor)
                 else:
-                    dic[letra] = int(fila[1])
+                    dic[letraC1] = int(valor)
     myKeys = list(dic.keys())
     myKeys.sort()
     dic = {i: dic[i] for i in myKeys}
     return dic
+
+if __name__ == "__main__":
+    print(pregunta_12())
